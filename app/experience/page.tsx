dce57993ea3c8bf61ef47090cdff4aaedc6d2a2d
Reinhardt Lagat <renhardtlagat@gmail.com>
2023-10-20 20:07:15 +0300

    final changes

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -20,33 +20,33 @@ const Experience = () => {
                Clinical research studies
              </p>
           </div>
-          <p className='text-blue-300 text-xs pt-1'>April 2023-Sept 2023</p>
+          <p className='text-blue-300 text-xs pt-1'>Sept 2023 - ct 2023</p>
           <p className='text-gray-400 pt-2'>
-          I hope this message finds you well. I am writing to express my keen interest in the software engineer position at SportServe. With a strong background in software development and a passion for solving complex problems, I am excited about the opportunity to contribute my skills and expertise to your team.
+            I worked as a lead software engineer in designing and building a research system. We used rescent technoligies which included NextS, Prisma, MySQL database, Tailwind and much more.
           </p>
         </div>
         <div>
          <div className='flex justify-start items-center'>
-             <h1 className='text-blue-200 font-semibold md:text-xl pr-2 text-lg'> Software Engineer</h1>
+             <h1 className='text-blue-200 font-semibold md:text-xl pr-2 text-lg'> Software Engineer Intern</h1>
              <p className='text-blue-300 '>
                Posta Kenya
              </p>
           </div>
-          <p className='text-blue-300 text-xs pt-1'>April 2023-Sept 2023</p>
+          <p className='text-blue-300 text-xs pt-1'>April 2023 - Aug 2023</p>
           <p className='text-gray-400 pt-2'>
-          I hope this message finds you well. I am writing to express my keen interest in the software engineer position at SportServe. With a strong background in software development and a passion for solving complex problems, I am excited about the opportunity to contribute my skills and expertise to your team
+           I worked as a software Engineer Intern in a team of developers, I worked with the team in building, maintaining and improving the present posta system. We used array of technologies including javascript, css and html.
           </p>
         </div>
         <div>
         <div className='flex justify-start items-center'>
-             <h1 className='text-blue-200 font-semibold md:text-xl pr-2 text-lg'> System Engineer</h1>
+             <h1 className='text-blue-200 font-semibold md:text-xl pr-2 text-lg'> System Engineer Intern</h1>
              <p className='text-blue-300 '>
                Rift Valley Training Institute
              </p>
           </div>
-          <p className='text-blue-300 text-xs pt-1'>April 2023-Sept 2023</p>
+          <p className='text-blue-300 text-xs pt-1'>April 2022-Aug 2022</p>
           <p className='text-gray-400 pt-2'>
-          I hope this message finds you well. I am writing to express my keen interest in the software engineer position at SportServe. With a strong background in software development and a passion for solving complex problems, I am excited about the opportunity to contribute my skills and expertise to your team
+           I worked with a team to maintain and improve the student management system at the school. We delved into not only coding but also brainstoming and finding solutions for various problems we had to solve in maintaining the system. We also delved into integrating finger print management system in the school. We used array of tools and technologies including Javascript, Css and HTML, Bootstrap.
           </p>
         </div>
         <div>
@@ -56,9 +56,9 @@ const Experience = () => {
                Upwork
              </p>
           </div>
-          <p className='text-blue-300 text-xs pt-1'>April 2023-Sept 2023</p>
+          <p className='text-blue-300 text-xs pt-1'>April 2023 - present</p>
           <p className='text-gray-400 pt-2'>
-          I hope this message finds you well. I am writing to express my keen interest in the software engineer position at SportServe. With a strong background in software development and a passion for solving complex problems, I am excited about the opportunity to contribute my skills and expertise to your team
+            Developed and designed websites and web apps for clients all over the world using array of technoligies in my know how. My experience as a freelancer has influenced my career as a developer in a special way, building array of projects, beating deadlines, working with clients of all types and at thesame time meeting customer expectations.
           </p>
         </div>
         <div className='pt-7'>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,151 +7,154 @@ import React from 'react'
 const Projects = () => {
   return (
     <div className='px-2 w-screen bg-slate-950 border-white h-full flex items-center justify-center'>
-      <div className='md:w-3/4 pr-3'>
+      <div className='md:w-3/4 pr-8 md:pr-16'>
            <div className='pt-'>
-            <h1 className='text-center font-semibold text-white text-3xl'>My recent work</h1>
+            <h1 className='text-center font-semibold text-blue-300 text-3xl pt-8 pb-3'>My recent work</h1>
           </div>
-        <div className='space-y-4  pt-10'>
+        <div className='space-y-4  pt-10 w-full'>
         <div className='md:px-10 px-2'>
-        <div className='flex-col md:flex-row flex relative'>
+           <div className='justify-start h-full w-full sm:w-auto flex pl-5 space-x-3 pb-2'>
+             <div className=''>
+              <Link href="https://quoteme-reinhardt254.vercel.app">
+                <ExternalLink size={24} color="white" fill="white" className=''/>
+              </Link>
+             </div>
+             <div className=''>
+              <Link href="https://github.com/Reinhardt254/Quote-Me">
+                 <Github size={24} color="white" fill="white" className=''/>
+              </Link>
+             </div>
+           </div>
+        <div className='flex-col md:flex-row flex relative max-sm:justify-center max-sm:items-center'>
             <Image 
              alt="project"
              src="/projects/quotes.png"
              width={400}
              height={200}
-             className='rounded'
+             className='rounded-t-md'
              
            />        
-           <div className='flex'>
-             <div className=''>
-              <Link href="">
-                <ExternalLink size={24} color="black" fill="black" className='absolute left-10 top-1'/>
-              </Link>
-             </div>
-             <div className=''>
-              <Link href="">
-                 <Github size={24} color="black" fill="black" className='top-1 absolute left-2'/>
-              </Link>
-             </div>
-           </div>
-          <p className='pt-2 text-base text-gray-400 md:pl-10'>
-          Discover the ultimate online shopping destination at [Your Website Name]. We are your one-stop destination for a wide range of high-quality products that cater to your every need. Whether you are on the hunt for fashion, electronics, home decor, or more, we have got you covered with a vast selection of curated items. Our user-friendly platform offers a seamless shopping experience, making it easy to find what you are looking for, explore new trends, and make secure, hassle-free transactions. With fast shipping, exceptional customer service, and a commitment to your satisfaction, [Your Website Name] is your trusted partner in e-commerce. Start exploring today and find the perfect products that enhance your lifestyle.
+          <p className='pt-2 text-base text-gray-400 md:pl-10 md:w-2/5 px-3 md:px-0'>
+             A quotes web app that implements CRUD functionalities. With a REST api built by NextJS server side and front end built with Tailwind. The web app allows users to read quotes, create their own accounts and write, delete or update quotes. The authentication was implemented using Next Auth.
           </p>
         </div>
-        <div>
-          <div className='pb-2 mt-3'>
-            <div className='flex space-x-3 font-semibold text-sm'>
-              <p className='bg-blue-800 rounded-3xl px-3 py-1 text-center text-gray-200'>Nextjs</p>
-              <p className='bg-blue-800 rounded-3xl px-3 py-1 text-center text-gray-200'>Tailwind</p>
-              <p className='bg-blue-800 rounded-3xl px-3 py-1 text-center text-gray-200'>Firebase</p>
+        <div className='w-full'>
+          <div className='pb-2 mt-0 '>
+            <div className='flex space-x-3 font-semibold text-sm flex-wrap md:w-2/5 shadow-xl shadow-blue-300 rounded-b-lg pb-3 px-3'>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300 mt-3 shadow-2xl shadow-blue-600'>Nextjs</p>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300 mt-3'>Tailwind</p>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300 mt-3'>MongoDB</p>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300 mt-3'>Mongoose</p>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300 mt-3'>NextAuth</p>
             </div>
             </div> 
             </div>
         </div>
-
         <div className='md:px-10 px-2'>
-        <div className='flex-col md:flex-row flex relative'>
+        <div className='justify-start h-full w-full sm:w-auto flex pl-5 space-x-3 pb-2'>
+             <div className=''>
+              <Link href="https://e-commerce-saas-admin-cj41d9r8c-reinhardt254.vercel.app">
+                <ExternalLink size={24} color="white" fill="white" className=''/>
+              </Link>
+             </div>
+             <div className=''>
+              <Link href="https://github.com/Reinhardt254/e-commerce-saas-Admin">
+                 <Github size={24} color="white" fill="white" className=''/>
+              </Link>
+             </div>
+           </div>
+        <div className='flex-col md:flex-row flex relative max-sm:justify-center max-sm:items-center'>
           <Image 
             alt="project"
             src="/projects/admin.png"
             width={400}
             height={200}
-            className='rounded'
-          />
-            <div className='flex'>
-             <div className=''>
-              <Link href="">
-                <ExternalLink size={24} color="black" fill="black" className='absolute left-10 top-1'/>
-              </Link>
-             </div>
-             <div className=''>
-              <Link href="">
-                 <Github size={24} color="black" fill="black" className='top-1 absolute left-2'/>
-              </Link>
-             </div>
-           </div>       
-          <p className='pt-2 text-base text-gray-400 md:pl-10'>
-          Discover the ultimate online shopping destination at [Your Website Name]. We are your one-stop destination for a wide range of high-quality products that cater to your every need. Whether you are on the hunt for fashion, electronics, home decor, or more, we have got you covered with a vast selection of curated items. Our user-friendly platform offers a seamless shopping experience, making it easy to find what you are looking for, explore new trends, and make secure, hassle-free transactions. With fast shipping, exceptional customer service, and a commitment to your satisfaction, [Your Website Name] is your trusted partner in e-commerce. Start exploring today and find the perfect products that enhance your lifestyle.
+            className='rounded-t-md'
+          />     
+          <p className='pt-2 text-base text-gray-400 md:pl-10 md:w-2/5 px-3 md:px-0'>
+            An Admin dashboard web application for an ecommerce store that implements all admin related tasks with a chart to manage statistics. The dashboard is built with NextJS and Prisma with a MySQL database i.e planet scale. The dashboard allows the admin to create a store or stores and they can add products with their attributes. The dashboard takes it futher by implementing apis for a creation of a client store.
           </p>
         </div>
         <div>
-          <div className='pb-2 mt-3'>
-            <div className='flex space-x-3 font-semibold text-sm'>
-              <p className='bg-blue-800 rounded-3xl px-3 py-1 text-center text-gray-200'>Nextjs</p>
-              <p className='bg-blue-800 rounded-3xl px-3 py-1 text-center text-gray-200'>Tailwind</p>
-              <p className='bg-blue-800 rounded-3xl px-3 py-1 text-center text-gray-200'>Firebase</p>
+          <div className='pb-2'>
+            <div className='flex space-x-3 font-semibold text-sm flex-wrap md:w-2/5 shadow-xl shadow-blue-300 rounded-b-lg pb-3 px-3'>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300 mt-3'>Nextjs</p>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300 mt-3'>Tailwind</p>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300 mt-3'>Prisma</p>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300 mt-3'>MySQL</p>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300 mt-3'>Clerk Auth</p>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300 mt-3'>Shadcn Ui</p>
             </div>
             </div> 
             </div>
         </div>
 
         <div className='md:px-10 px-2 '>
+        <div className='justify-start h-full w-full sm:w-auto flex pl-5 space-x-3 pb-2'>
+             <div className=''>
+              <Link href="https://helpingheartsfoundation.vercel.app">
+                <ExternalLink size={24} color="white" fill="white" className=''/>
+              </Link>
+             </div>
+             <div className=''>
+              <Link href="https://github.com/Reinhardt254/helpingheartsfoundation">
+                 <Github size={24} color="white" fill="white" className=''/>
+              </Link>
+             </div>
+           </div>
         <div className='flex-col md:flex-row flex relative'>  
           <Image 
             alt="project"
             src="/projects/helping.png"
             width={400}
             height={200}
-            className='rounded'
-          />
-           <div className='flex'>
-             <div className=''>
-              <Link href="">
-                <ExternalLink size={24} color="black" fill="black" className='absolute left-10 top-1'/>
-              </Link>
-             </div>
-             <div className=''>
-              <Link href="">
-                 <Github size={24} color="black" fill="black" className='top-1 absolute left-2'/>
-              </Link>
-             </div>
-           </div>    
-          <p className='pt-2 text-base text-gray-400 md:pl-10'>
-          Discover the ultimate online shopping destination at [Your Website Name]. We are your one-stop destination for a wide range of high-quality products that cater to your every need. Whether you are on the hunt for fashion, electronics, home decor, or more, we have got you covered with a vast selection of curated items. Our user-friendly platform offers a seamless shopping experience, making it easy to find what you are looking for, explore new trends, and make secure, hassle-free transactions. With fast shipping, exceptional customer service, and a commitment to your satisfaction, [Your Website Name] is your trusted partner in e-commerce. Start exploring today and find the perfect products that enhance your lifestyle.
+            className='rounded-t-md'
+          />  
+          <p className='pt-2 text-base text-gray-400 md:pl-10 md:w-2/5 px-3 md:px-0'>
+            A non-govermental organization website built with NextJs and Tailwind. The website is simple website that describes what the organization do and how to donate for their cause.
           </p>
         </div>
         <div>
           <div className='pb-2 mt-3'>
-            <div className='flex space-x-3 font-semibold text-sm'>
-              <p className='bg-blue-800 rounded-3xl px-3 py-1 text-center text-gray-200'>Nextjs</p>
-              <p className='bg-blue-800 rounded-3xl px-3 py-1 text-center text-gray-200'>Tailwind</p>
-              <p className='bg-blue-800 rounded-3xl px-3 py-1 text-center text-gray-200'>Firebase</p>
+            <div className='flex space-x-3 font-semibold text-sm flex-wrap md:w-2/5 shadow-xl shadow-blue-300 rounded-b-lg pb-3 px-3'>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300'>Nextjs</p>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300'>Tailwind</p>
             </div>
-            </div> 
+            </div>9
             </div>
         </div>
 
         <div className='md:px-10 px-2 '>
-        <div className='flex-col md:flex-row flex relative'>  
+        <div className='justify-start h-full w-full sm:w-auto flex pl-5 space-x-3 pb-2'>
+             <div className=''>
+              <Link href="https://g-p-3.vercel.app">
+                <ExternalLink size={24} color="white" fill="white" className=''/>
+              </Link>
+             </div>
+             <div className=''>
+              <Link href="https://github.com/Reinhardt254/g-p-3">
+                 <Github size={24} color="white" fill="white" className=''/>
+              </Link>
+             </div>
+           </div>
+        <div className='flex-col md:flex-row flex relative max-sm:justify-center max-sm:items-center'>  
           <Image 
             alt="project"
             src="/projects/gpt.png"
             width={400}
             height={200}
-            className='rounded'
+            className='rounded-t-md'
           />
-            <div className='flex'>
-             <div className=''>
-              <Link href="">
-                <ExternalLink size={24} color="white" fill="white" className='absolute left-10 top-1'/>
-              </Link>
-             </div>
-             <div className=''>
-              <Link href="">
-                 <Github size={24} color="white" fill="white" className='top-1 absolute left-2'/>
-              </Link>
-             </div>
-           </div>  
-          <p className='pt-2 text-base text-gray-400 md:pl-10'>
-          Discover the ultimate online shopping destination at [Your Website Name]. We are your one-stop destination for a wide range of high-quality products that cater to your every need. Whether you are on the hunt for fashion, electronics, home decor, or more, we have got you covered with a vast selection of curated items. Our user-friendly platform offers a seamless shopping experience, making it easy to find what you are looking for, explore new trends, and make secure, hassle-free transactions. With fast shipping, exceptional customer service, and a commitment to your satisfaction, [Your Website Name] is your trusted partner in e-commerce. Start exploring today and find the perfect products that enhance your lifestyle.
+          <p className='pt-2 text-base text-gray-400 md:pl-10 md:w-2/5 px-3 md:px-0'>
+            A website with eye catching ui built with React and SAAS.
+            The website is inspired with the recent rise of AI technoligies including chatgpt. The website is responsive in all applications with a working navbar in both large and small devices
           </p>
         </div>
         <div>
           <div className=' mt-3 pb-10'>
-            <div className='flex space-x-3 font-semibold text-sm'>
-              <p className='bg-blue-800 rounded-3xl px-3 py-1 text-center text-gray-200'>Nextjs</p>
-              <p className='bg-blue-800 rounded-3xl px-3 py-1 text-center text-gray-200'>Tailwind</p>
-              <p className='bg-blue-800 rounded-3xl px-3 py-1 text-center text-gray-200'>Firebase</p>
+            <div className='flex space-x-3 font-semibold text-sm flex-wrap md:w-2/5 shadow-xl shadow-blue-300 rounded-b-lg pb-3 px-3'>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300'>React</p>
+              <p className='bg-blue-900 rounded-3xl px-3 py-1 text-center text-gray-300'>SAAS</p>
             </div>
             </div> 
             </div>
