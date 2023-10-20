3760265dcee5522f119d2e57876e504e8404d2f1
Reinhardt Lagat <renhardtlagat@gmail.com>
2023-10-20 21:50:34 +0300

    G

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,4 @@
 import About from '@/components/about'
-import Contact from '@/components/contact'
-import Stack from '@/app/skills/page'
-import Projects from '@/app/projects/page'
-import React from 'react'
 
 const Page = () => {
   return (
