732077120b8b006a52295c1e95da6d6ad817171d
Reinhardt Lagat <56647015+Reinhardt254@users.noreply.github.com>
2024-08-28 10:17:31 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-      publicRoutes : ["/" , "/contact" , "/experience", "/projects", "/skills", "/api/messages"]
+      ignoreRoutes : ["/" , "/contact" , "/experience", "/projects", "/skills", "/api/messages"]
 });
  
 export const config = {
