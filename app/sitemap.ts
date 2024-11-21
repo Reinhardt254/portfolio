dcec8398ae3fca84540e80d849e9a6fc0fd2f7d1
Reinhardt Lagat <56647015+Reinhardt254@users.noreply.github.com>
2024-11-21 08:40:09 +0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,31 +1,31 @@
 export default function sitemap() {
    return [
      {
-       url: 'https://reinhardtdev.com/',
+       url: 'https://www.reinhardtdev.com/',
        lastModified: new Date(),
        changeFrequency: 'yearly',
        priority: 1,
      },
      {
-       url: 'https://reinhardtdev.com/about',
+       url: 'https://www.reinhardtdev.com/skills',
        lastModified: new Date(),
        changeFrequency: 'monthly',
        priority: 0.8,
      },
      {
-       url: 'https://reinhardtdev.com/projects',
+       url: 'https://www.reinhardtdev.com/projects',
        lastModified: new Date(),
        changeFrequency: 'weekly',
        priority: 0.5,
      },
      {
-      url: 'https://reinhardtdev.com/experience',
+      url: 'htthttps://www.reinhardtdev.com/experience',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
     },
     {
-      url: 'https://reinhardtdev.com/contact',
+      url: 'https://www.reinhardtdev.com/contact',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
