58ed5a2262538282580bead8c6b43d32da822877
Reinhardt Lagat <56647015+Reinhardt254@users.noreply.github.com>
2024-11-22 08:16:37 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,6 @@ export default function robots() {
        allow: '/',
        disallow: '/dashboard', 
      },
-     sitemap: 'https://www.reinhardtdev.com/sitemap.xml',
+     sitemap: 'https://reinhardtdev.com/sitemap.xml',
    }
  }
